628a29eb79653999f5eeaf45f1e6d1927d204beb
yanghan-microsoft <hanyang@microsoft.com>
2024-12-27 14:08:23 +0800

    add relax subcommand

--- src/mattersim/cli/mattersim_app.py
+++ src/mattersim/cli/mattersim_app.py
@@ -2,42 +2,40 @@ import argparse
 import os
 import uuid
 from collections import defaultdict
+from datetime import datetime
 from typing import List, Union
 
 # import numpy as np
 import pandas as pd
 from ase import Atoms
-
-# from ase.constraints import Filter
+from ase.constraints import Filter
 from ase.io import read as ase_read
-
-# from ase.optimize.optimize import Optimizer
+from ase.optimize.optimize import Optimizer
 from ase.units import GPa
 from loguru import logger
 from pymatgen.io.ase import AseAtomsAdaptor
 from tqdm import tqdm
 
 # from mattersim.applications.phonon import PhononWorkflow
-# from mattersim.applications.relax import Relaxer
+from mattersim.applications.relax import Relaxer
 from mattersim.forcefield import MatterSimCalculator
 
 __all__ = ["singlepoint", "phonon", "relax", "moldyn"]
 
 
 def singlepoint(
-    structure_file: Union[str, List[str]],
+    atoms_list: List[Atoms],
+    *,
     mattersim_model: str,
     device: str = "cpu",
     work_dir: str = str(uuid.uuid4()),
     save_csv: str = "results.csv.gz",
-):
+    **kwargs,
+) -> dict:
     """
     Predict single point properties for a list of atoms.
 
     """
-    atoms_list = parse_atoms_list(structure_file, mattersim_model, device)
-    logger.info(f"Predicting single point properties for {len(atoms_list)} structures.")
-
     logger.info("Predicting single point properties.")
     predicted_properties = defaultdict(list)
     for atoms in tqdm(
@@ -59,20 +57,123 @@ def singlepoint(
 
     df = pd.DataFrame(predicted_properties)
     df.to_csv(os.path.join(work_dir, save_csv), index=False)
+    return predicted_properties
 
 
-def singlepoint_cli(args: argparse.Namespace):
-    singlepoint(
-        args.structure_file,
-        args.mattersim_model,
-        args.device,
-        args.work_dir,
-        args.save_csv,
+def singlepoint_cli(args: argparse.Namespace) -> dict:
+    """
+    CLI wrapper for singlepoint function.
+
+    Args:
+        args (argparse.Namespace): Command line arguments.
+
+    Returns:
+        dict: Dictionary containing the predicted properties.
+
+    """
+    atoms_list = parse_atoms_list(
+        args.structure_file, args.mattersim_model, args.device
+    )
+    return singlepoint(atoms_list, **vars(args))
+
+
+def relax(
+    atoms_list: List[Atoms],
+    *,
+    optimizer: Union[str, Optimizer] = "FIRE",
+    filter: Union[str, Filter, None] = None,
+    constrain_symmetry: bool = False,
+    fix_axis: Union[bool, List[bool]] = False,
+    pressure_in_GPa: float = None,
+    fmax: float = 0.01,
+    steps: int = 500,
+    work_dir: str = str(uuid.uuid4()),
+    save_csv: str = "results.csv.gz",
+    **kwargs,
+) -> dict:
+    """
+    Relax a list of atoms structures.
+
+    Args:
+        atoms_list (List[Atoms]): List of ASE Atoms objects.
+        optimizer (Union[str, Optimizer]): The optimizer to use. Default is "FIRE".
+        filter (Union[str, Filter, None]): The filter to use.
+        constrain_symmetry (bool): Whether to constrain symmetry. Default is False.
+        fix_axis (Union[bool, List[bool]]): Whether to fix the axis. Default is False.
+        pressure_in_GPa (float): Pressure in GPa to use for relaxation.
+        fmax (float): Maximum force tolerance for relaxation. Default is 0.01.
+        steps (int): Maximum number of steps for relaxation. Default is 500.
+        work_dir (str): Working directory for the calculations.
+            Default is a UUID with timestamp.
+        save_csv (str): Save the results to a CSV file. Default is `results.csv.gz`.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the relaxed results.
+    """
+    params_filter = {}
+
+    if pressure_in_GPa:
+        params_filter["scalar_pressure"] = (
+            pressure_in_GPa * GPa
+        )  # convert GPa to eV/Angstrom^3
+        filter = "ExpCellFilter" if filter is None else filter
+    elif filter:
+        params_filter["scalar_pressure"] = 0.0
+
+    relaxer = Relaxer(
+        optimizer=optimizer,
+        filter=filter,
+        constrain_symmetry=constrain_symmetry,
+        fix_axis=fix_axis,
     )
 
+    relaxed_results = defaultdict(list)
+    for atoms in tqdm(atoms_list, total=len(atoms_list), desc="Relaxing structures"):
+        converged, relaxed_atoms = relaxer.relax(
+            atoms,
+            params_filter=params_filter,
+            fmax=fmax,
+            steps=steps,
+        )
+        relaxed_results["converged"].append(converged)
+        relaxed_results["structure"].append(
+            AseAtomsAdaptor.get_structure(relaxed_atoms).to_json()
+        )
+        relaxed_results["energy"].append(relaxed_atoms.get_potential_energy())
+        relaxed_results["energy_per_atom"].append(
+            relaxed_atoms.get_potential_energy() / len(relaxed_atoms)
+        )
+        relaxed_results["forces"].append(relaxed_atoms.get_forces())
+        relaxed_results["stress"].append(relaxed_atoms.get_stress(voigt=False))
+        relaxed_results["stress_GPa"].append(
+            relaxed_atoms.get_stress(voigt=False) / GPa
+        )
 
-def relax(args: argparse.Namespace):
-    pass
+        logger.info(f"Relaxed structure: {relaxed_atoms}")
+
+    if not os.path.exists(work_dir):
+        os.makedirs(work_dir)
+
+    logger.info(f"Saving the results to {os.path.join(work_dir, save_csv)}")
+    df = pd.DataFrame(relaxed_results)
+    df.to_csv(os.path.join(work_dir, save_csv), index=False)
+    return relaxed_results
+
+
+def relax_cli(args: argparse.Namespace) -> dict:
+    """
+    CLI wrapper for relax function.
+
+    Args:
+        args (argparse.Namespace): Command line arguments.
+
+    Returns:
+        dict: Dictionary containing the relaxed results
+    """
+    atoms_list = parse_atoms_list(
+        args.structure_file, args.mattersim_model, args.device
+    )
+    return relax(atoms_list, **vars(args))
 
 
 def phonon():
@@ -107,8 +208,9 @@ def add_common_args(parser: argparse.ArgumentParser):
     parser.add_argument(
         "--work-dir",
         type=str,
-        default=str(uuid.uuid4()),
-        help="Working directory for the calculations. Defaults to a UUID when not set.",
+        default=datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + "-" + str(uuid.uuid4()),
+        help="Working directory for the calculations. "
+        "Defaults to a UUID with timestamp when not set.",
     )
     parser.add_argument(
         "--save-csv",
@@ -150,6 +252,55 @@ def main():
     )
     add_common_args(singlepoint_parser)
     singlepoint_parser.set_defaults(func=singlepoint_cli)
+
+    # Sub-command for relax
+    relax_parser = subparsers.add_parser(
+        "relax", help="Relax a list of atoms structures."
+    )
+    add_common_args(relax_parser)
+    relax_parser.add_argument(
+        "--optimizer",
+        type=str,
+        default="FIRE",
+        help="The optimizer to use. Default is FIRE.",
+    )
+    relax_parser.add_argument(
+        "--filter",
+        type=str,
+        default=None,
+        help="The filter to use.",
+    )
+    relax_parser.add_argument(
+        "--constrain-symmetry",
+        action="store_true",
+        help="Constrain symmetry.",
+    )
+    relax_parser.add_argument(
+        "--fix-axis",
+        type=bool,
+        default=False,
+        nargs="+",
+        help="Fix the axis.",
+    )
+    relax_parser.add_argument(
+        "--pressure-in-GPa",
+        type=float,
+        default=None,
+        help="Pressure in GPa to use for relaxation.",
+    )
+    relax_parser.add_argument(
+        "--fmax",
+        type=float,
+        default=0.01,
+        help="Maximum force tolerance for relaxation.",
+    )
+    relax_parser.add_argument(
+        "--steps",
+        type=int,
+        default=500,
+        help="Maximum number of steps for relaxation.",
+    )
+    relax_parser.set_defaults(func=relax_cli)
 
     # Parse arguments
     args = argparser.parse_args()
