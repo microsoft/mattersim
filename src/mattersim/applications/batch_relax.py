f11986effe0350c200c5ead115ac5bbbb1efd936
yanghan-microsoft <hanyang@microsoft.com>
2025-01-05 11:54:30 +0800

    fix bug and clean up codes

--- src/mattersim/applications/batch_relax.py
+++ src/mattersim/applications/batch_relax.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import logging
 import sys
 from typing import Dict, List, Union
 
@@ -9,19 +8,11 @@ from ase.constraints import Filter
 from ase.filters import ExpCellFilter, FrechetCellFilter
 from ase.optimize import BFGS, FIRE
 from ase.optimize.optimize import Optimizer
+from loguru import logger
 from tqdm import tqdm
 
 from mattersim.datasets.utils.build import build_dataloader
 from mattersim.forcefield.potential import Potential
-
-FORMAT = "[%(levelname)s] [%(name)s]: %(message)s"
-logging.basicConfig(
-    level=logging.INFO,
-    format=FORMAT,
-    # use stdout to flush tqdm out periodically
-    stream=sys.stdout,
-)
-LOG = logging.getLogger(__name__)
 
 
 class DummyBatchCalculator(Calculator):
@@ -38,7 +29,7 @@ class DummyBatchCalculator(Calculator):
         return atoms.arrays["forces"]
 
     def get_stress(self, atoms=None):
-        return units.GPa*atoms.info["stress"]
+        return units.GPa * atoms.info["stress"]
 
 
 class BatchRelaxer(object):
@@ -54,24 +45,29 @@ class BatchRelaxer(object):
     def __init__(
         self,
         potential: Potential,
-        optimizer: Union[Optimizer, str] = "FIRE",
-        filter: Union[Filter, str, None] = None,
+        optimizer: Union[str, type[Optimizer]] = "FIRE",
+        filter: Union[type[Filter], str, None] = None,
         fmax: float = 0.05,
         max_natoms_per_batch: int = 512,
-        ):
+    ):
         self.potential = potential
         self.device = potential.device
-        self.optimizer = (
-            self.SUPPORTED_OPTIMIZERS[optimizer.upper()]
-            if isinstance(optimizer, str)
-            else optimizer
-        )
-        if filter is not None:
-            self.filter = (
-                self.SUPPORTED_FILTERS[filter.upper()]
-                if isinstance(filter, str)
-                else filter
-            )
+        if isinstance(optimizer, str):
+            if optimizer.upper() not in self.SUPPORTED_OPTIMIZERS:
+                raise ValueError(f"Unsupported optimizer: {optimizer}")
+            self.optimizer = self.SUPPORTED_OPTIMIZERS[optimizer.upper()]
+        elif issubclass(optimizer, Optimizer):
+            self.optimizer = optimizer
+        else:
+            raise ValueError(f"Unsupported optimizer: {optimizer}")
+        if isinstance(filter, str):
+            if filter.upper() not in self.SUPPORTED_FILTERS:
+                raise ValueError(f"Unsupported filter: {filter}")
+            self.filter = self.SUPPORTED_FILTERS[filter.upper()]
+        elif filter is None or issubclass(filter, Filter):
+            self.filter = filter
+        else:
+            raise ValueError(f"Unsupported filter: {filter}")
         self.fmax = fmax
         self.max_natoms_per_batch = max_natoms_per_batch
         self.optimizer_instances: List[Optimizer] = []
@@ -83,7 +79,7 @@ class BatchRelaxer(object):
     def insert(self, atoms: Atoms):
         atoms.set_calculator(DummyBatchCalculator())
         optimizer_instance = self.optimizer(
-            self.filter(atoms),
+            self.filter(atoms) if self.filter else atoms
         )
         optimizer_instance.fmax = self.fmax
         self.optimizer_instances.append(optimizer_instance)
@@ -91,45 +87,54 @@ class BatchRelaxer(object):
 
     def step_batch(self):
         atoms_list = []
-        for idx, fire in enumerate(self.optimizer_instances):
+        for idx, opt in enumerate(self.optimizer_instances):
             if self.is_active_instance[idx]:
-                atoms_list.append(fire.atoms.atoms)
+                atoms_list.append(opt.atoms)
 
-        # Note: we use a batch size of len(atoms_list) because we only want to run one batch at a time
-        dataloader = build_dataloader(atoms_list, batch_size=len(atoms_list), only_inference=True)
+        # Note: we use a batch size of len(atoms_list)
+        # because we only want to run one batch at a time
+        dataloader = build_dataloader(
+            atoms_list, batch_size=len(atoms_list), only_inference=True
+        )
         energy_batch, forces_batch, stress_batch = self.potential.predict_properties(
             dataloader, include_forces=True, include_stresses=True
         )
 
         counter = 0
         self.finished = True
-        for idx, fire in enumerate(self.optimizer_instances):
+        for idx, opt in enumerate(self.optimizer_instances):
             if self.is_active_instance[idx]:
-                # Set the properties so the dummy calculator can return them within the optimizer step
-                fire.atoms.atoms.info["total_energy"] = energy_batch[counter]
-                fire.atoms.atoms.arrays["forces"] = forces_batch[counter]
-                fire.atoms.atoms.info["stress"] = stress_batch[counter]
+                # Set the properties so the dummy calculator can
+                # return them within the optimizer step
+                opt.atoms.info["total_energy"] = energy_batch[counter]
+                opt.atoms.arrays["forces"] = forces_batch[counter]
+                opt.atoms.info["stress"] = stress_batch[counter]
                 try:
-                    self.trajectories[fire.atoms.atoms.info["structure_index"]].append(fire.atoms.atoms.copy())
+                    self.trajectories[opt.atoms.info["structure_index"]].append(
+                        opt.atoms.copy()
+                    )
                 except KeyError:
-                    self.trajectories[fire.atoms.atoms.info["structure_index"]] = [fire.atoms.atoms.copy()]
+                    self.trajectories[opt.atoms.info["structure_index"]] = [
+                        opt.atoms.copy()
+                    ]
 
-                fire.step()
-                if fire.converged():
+                opt.step()
+                if opt.converged():
                     self.is_active_instance[idx] = False
                     self.total_converged += 1
                     if self.total_converged % 100 == 0:
-                        LOG.info(f"Relaxed {self.total_converged} structures.")
+                        logger.info(f"Relaxed {self.total_converged} structures.")
                 else:
                     self.finished = False
                 counter += 1
 
         # remove inactive instances
         self.optimizer_instances = [
-            fire for fire, active in zip(self.optimizer_instances, self.is_active_instance) if active
+            opt
+            for opt, active in zip(self.optimizer_instances, self.is_active_instance)
+            if active
         ]
         self.is_active_instance = [True] * len(self.optimizer_instances)
-
 
     def relax(
         self,
@@ -147,11 +152,15 @@ class BatchRelaxer(object):
             pointer < len(atoms_list) or not self.finished
         ):  # While there are unfinished instances or atoms left to insert
             while pointer < len(atoms_list) and (
-                sum([len(atoms.atoms.atoms) for atoms in self.optimizer_instances])
+                sum([len(atoms.atoms) for atoms in self.optimizer_instances])
                 + len(atoms_list[pointer])
                 <= self.max_natoms_per_batch
-            ):  # While there are enough n_atoms slots in the batch and we have not reached the end of the list.
-                self.insert(atoms_list_[pointer])  #  Insert new structure to fire instances
+            ):
+                # While there are enough n_atoms slots in the
+                # batch and we have not reached the end of the list.
+                self.insert(
+                    atoms_list_[pointer]
+                )  # Insert new structure to fire instances
                 self.tqdmcounter.update(1)
                 pointer += 1
             self.step_batch()
